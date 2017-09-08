bf041f9b97c485a69f5961c8108b05d95961bf3c
VectorLu <vectorpinglu@outlook.com>
2017-09-08 12:13:48 +0800

    二叉树的最大值

--- 632_binaryTreeMaximumNode.py
+++ 632_binaryTreeMaximumNode.py
@@ -1,21 +1,21 @@
 class Solution:
     # @param {TreeNode} root the root of binary tree
     # @return {TreeNode} the max node
-    node = None
-    maxNum = -99999
-    
+    node = TreeNode(-9999)
+
     def maxNode(self, root):
         # Write your code here
         if root is None:
             return None
-        self.max(root)
-        return self.node
-    
+        else:
+            self.max(root)
+            return self.node
+
     def max(self, root):
         if root is None:
             return None
-        if root.val > self.maxNum:
+        if root.val > self.node.val:
             self.node = root
-            self.maxNum = root.val
         self.max(root.left)
         self.max(root.right)
+        
